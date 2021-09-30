b59c22f30ccacecd09e5a397318b8a2624dd983c
muhoz <71699176+muhoz@users.noreply.github.com>
2021-09-30 16:05:23 +0100

    Update PIR_Motion_Sensor.ino

--- PIR_Motion_Sensor.ino
+++ PIR_Motion_Sensor.ino
@@ -1,45 +1,22 @@
-/*  
-    Arduino with PIR motion sensor
-    For complete project details, visit: http://RandomNerdTutorials.com/pirsensor
-    Modified by Rui Santos based on PIR sensor by Limor Fried
-*/
-
-
- 
-int led = 2;                // the pin that the LED is atteched to
-int sensor = 6;              // the pin that the sensor is atteched to
-int state = LOW;             // by default, no motion detected
-int val = 0;                 // variable to store the sensor status (value)
+//declartion of variables  
+int pirPin = 6;              // define the pin where the pir is connected to
+int state = LOW;             // state of the PIR motion sensor
+                             // LOW : A motion has been detected
+                             // HIGH : There is no motion detected
 
 void setup() {
-  pinMode(led, OUTPUT);      // initalize LED as an output
-  pinMode(sensor, INPUT);    // initialize sensor as an input
-  Serial.begin(9600);        // initialize serial
+  pinMode(sensor, INPUT);    // initialize the PIR as an input
+  Serial.begin(9600);        // open the Serial port(communication between Arduino and the USB port of the computer)
 }
 
 void loop(){
-//  val = digitalRead(sensor);   // read sensor value
-//  if (val == HIGH) {           // check if the sensor is HIGH
-//    digitalWrite(led, HIGH);   // turn LED ON
-//    delay(100);                // delay 100 milliseconds 
-    
+  val = digitalRead(sensor);   // read sensor value
     if (state == LOW) {
-      Serial.println("Motion detected!"); 
-      state = HIGH;       // update variable state to HIGH
+      Serial.println("A motion detected!"); 
+      //Do whatever you want
+    }   
+    else if (state == HIGH){
+      Serial.println("No motion!");
+      //Do whatever you want 
     }
-  //} 
-//  else {
-//      digitalWrite(led, LOW); // turn LED OFF
-//      delay(200);             // delay 200 milliseconds 
-//      
-      if (state == HIGH){
-        Serial.println("Motion stopped!");
-        state = LOW;       // update variable state to LOW
-    }
-  //}
-  digitalWrite(led,HIGH);
-  delay(3000);
-  digitalWrite(led,LOW);
-  delay(3000);
-  Serial.println("Toto");
 }
